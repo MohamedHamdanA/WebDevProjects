95d3a1d6123c426bad670edfa770f12c2334db23
MohamedHamdanA <mdhamdan1111@gmail.com>
2024-12-28 20:41:46 +0530

    Initial commit

---
+++ WeatherViewer/README.md
@@ -0,0 +1,6 @@
+# Weather Viewer
+
+This is basic weather viewer website where we can see weather of various city by using search bar.
+
+Weather Details are got from openweathermap.org using api key.
+
---
+++ WeatherViewer/src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import SearchBar from "./components/SearchBar";
+import WeatherCard from "./components/WeatherCard";
+import "./App.css";
+
+const App = () => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState("");
+
+  const fetchWeather = async (city) => {
+    try {
+      const apiKey = "API KEY";
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+      );
+      setWeatherData(response.data);
+      setError("");
+    } catch (err) {
+      console.log(err);
+      setError("City not found. Please try again.");
+      setWeatherData(null);
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="app-container">
+        <h1>Weather App</h1>
+        <SearchBar fetchWeather={fetchWeather} />
+        {error && <p className="error">{error}</p>}
+        {weatherData && <WeatherCard data={weatherData} />}
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
